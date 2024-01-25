5ac59dd445138c2d17cefa21564f62c96034867c
Khobe Arzadon <khobearzadon@yahoo.com>
2024-01-25 15:23:02 -0800

    add user authentication express middleware

--- airbnb/backend/routes/api/index.js
+++ airbnb/backend/routes/api/index.js
@@ -1,8 +1,8 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// GET /api/restore-user
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
 
 module.exports = router;
