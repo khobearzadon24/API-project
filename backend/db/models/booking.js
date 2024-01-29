4f184ad66767fd2f7ac3cb12ef7275149471c745
Khobe Arzadon <khobearzadon@yahoo.com>
2024-01-29 14:17:42 -0800

    made sure startdate and enddate work correctly

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -22,12 +22,18 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       startDate: {
-        type: DataTypes.DATE,
+        type: DataTypes.DATEONLY,
         allowNull: false,
+        validate: {
+          isBefore: endDate,
+        },
       },
       endDate: {
-        type: DataTypes.DATE,
+        type: DataTypes.DATEONLY,
         allowNull: false,
+        validate: {
+          isAfter: startDate,
+        },
       },
     },
     {
