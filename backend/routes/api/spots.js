6ab368efd005db80c8377a969ed197ff2fee2220
Khobe Arzadon <khobearzadon@yahoo.com>
2024-01-30 13:33:12 -0800

    get all users

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -23,47 +23,93 @@ const router = express.Router();
 
 //get all the spots
 router.get("/", async (req, res) => {
-  const allSpots = await Spot.findAll();
+  //   const numReviews = await Review.count({
+  //     spot: "spotId",
+  //   });
 
-  const preview = await SpotImage.findAll({
-    where: {
-      spotId: allSpots.id,
-    },
-  });
+  //   const getAllSpotImages = await SpotImage.findAll({
+  //     spot: "spotId",
+  //   });
 
-  const reviews = await Review.findAll({
-    where: {
-      spotId: allSpots.id,
-    },
-  });
-  //calculate the total amount of reviews
-  const reviewCount = reviews.length;
+  const getAllSpots = await Spot.findAll();
 
-  //calculate the total review number
-  let reviewTotalAmount = 0;
-  reviews.forEach((review) => {
-    reviewTotalAmount += review.stars;
-  });
+  let avgRating;
 
-  const reviewAvg = Math.round(reviewTotalAmount / reviewCount);
+  for (let i = 0; i < getAllSpots.length; i++) {
+    let numReviews = await Review.count({
+      where: {
+        spotId: getAllSpots[i].id,
+      },
+    });
+    let stars = await Review.sum("stars", {
+      where: {
+        spotId: getAllSpots[i].id,
+      },
+    });
 
-  const payload = {
-    id: allSpots.id,
-    ownerId: allSpots.ownerId,
-    address: allSpots.address,
-    city: allSpots.city,
-    country: allSpots.country,
-    lat: allSpots.lat,
-    lng: allSpots.lng,
-    name: allSpots.name,
-    description: allSpots.description,
-    price: allSpots.price,
-    createdAt: allSpots.createdAt,
-    updatedAt: allSpots.createdAt,
-    avgRating: reviewAvg,
-    previewImage: preview.url,
-  };
-  res.json(payload);
+    if (stars === null) avgRating = 0;
+    else {
+      avgRating = stars / numReviews;
+    }
+
+    getAllSpots[i].setDataValue("avgRating", avgRating);
+
+    const imageUrl = await SpotImage.findOne({
+      where: {
+        spotId: getAllSpots[i].id,
+      },
+    });
+
+    if (imageUrl === null) {
+      getAllSpots[i].setDataValue("previewImage", null);
+    } else {
+      getAllSpots[i].setDataValue("previewImage", imageUrl.url);
+    }
+  }
+  res.json(getAllSpots);
 });
+// router.get("/", async (req, res) => {
+//   const allSpots = await Spot.findAll();
+
+//   const preview = await SpotImage.findAll({
+//     where: {
+//       spotId: allSpots.id,
+//     },
+//   });
+
+//   const reviews = await Review.findAll({
+//     where: {
+//       spotId: allSpots.id,
+//     },
+//   });
+//   //calculate the total amount of reviews
+//   const reviewCount = reviews.length;
+
+//   //calculate the total review number
+//   let reviewTotalAmount = 0;
+//   reviews.forEach((review) => {
+//     reviewTotalAmount += review.stars;
+//   });
+//   //average rating
+//   const reviewAvg = Math.round(reviewTotalAmount / reviewCount);
+
+//   const payload = {
+//     id: allSpots.id,
+//     ownerId: allSpots.ownerId,
+//     address: allSpots.address,
+//     city: allSpots.city,
+//     country: allSpots.country,
+//     lat: allSpots.lat,
+//     lng: allSpots.lng,
+//     name: allSpots.name,
+//     description: allSpots.description,
+//     price: allSpots.price,
+//     createdAt: allSpots.createdAt,
+//     updatedAt: allSpots.createdAt,
+//     avgRating: reviewAvg,
+//     previewImage: preview.url,
+//   };
+//   res.json(payload);
+// });
 
 module.exports = router;
