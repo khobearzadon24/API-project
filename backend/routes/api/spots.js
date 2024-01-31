d36628a3a9cc9d478c2dbe02cf468c4920c3c292
Khobe Arzadon <khobearzadon@yahoo.com>
2024-01-30 16:56:36 -0800

    get details of a spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -7,9 +7,10 @@ const {
   restoreUser,
   requireAuth,
 } = require("../../utils/auth");
-const { Spot, SpotImage, Review } = require("../../db/models");
+const { Spot, SpotImage, Review, User } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const spot = require("../../db/models/spot");
 
 const router = express.Router();
 
@@ -24,6 +25,101 @@ const router = express.Router();
 //     .withMessage("Please provide a password."),
 //   handleValidationErrors,
 // ];
+router.get("/current", requireAuth, async (req, res) => {
+  const ownerId = req.user.id;
+  const getAllSpotsByOwner = await Spot.findAll({
+    where: {
+      ownerId,
+    },
+  });
+  let avgRating;
+
+  for (let i = 0; i < getAllSpotsByOwner.length; i++) {
+    let numReviews = await Review.count({
+      where: {
+        spotId: getAllSpotsByOwner[i].id,
+      },
+    });
+    let stars = await Review.sum("stars", {
+      where: {
+        spotId: getAllSpotsByOwner[i].id,
+      },
+    });
+
+    if (stars === null) avgRating = 0;
+    else {
+      avgRating = stars / numReviews;
+    }
+
+    getAllSpotsByOwner[i].setDataValue("avgRating", avgRating);
+
+    const imageUrl = await SpotImage.findOne({
+      where: {
+        spotId: getAllSpotsByOwner[i].id,
+      },
+    });
+
+    if (imageUrl === null) {
+      getAllSpotsByOwner[i].setDataValue("previewImage", null);
+    } else {
+      getAllSpotsByOwner[i].setDataValue("previewImage", imageUrl.url);
+    }
+  }
+  res.json(getAllSpotsByOwner);
+});
+
+//get details of a spot from an id
+router.get("/:spotId", async (req, res) => {
+  const { spotId } = req.params;
+
+  const spotDetail = await Spot.findByPk(spotId);
+  if (spotDetail === null) {
+    return res.status(404).json({
+      message: `Spot couldn't be found.`,
+    });
+  }
+
+  let avgRating;
+
+  let numReviews = await Review.count({
+    where: {
+      spotId: spotId,
+    },
+  });
+  let stars = await Review.sum("stars", {
+    where: {
+      spotId: spotId,
+    },
+  });
+
+  if (stars === null) avgRating = 0;
+  else {
+    avgRating = stars / numReviews;
+  }
+  spotDetail.setDataValue("numReviews", numReviews);
+  spotDetail.setDataValue("avgRating", avgRating);
+
+  const imageUrl = await SpotImage.findAll({
+    where: {
+      spotId: spotId,
+    },
+    attributes: ["id", "url", "preview"],
+  });
+
+  if (imageUrl === null) {
+    spotDetail.setDataValue("previewImage", null);
+  } else {
+    spotDetail.setDataValue("previewImage", imageUrl);
+  }
+
+  const owner = await User.findByPk(spotDetail.ownerId, {
+    attributes: ["id", "firstName", "lastName"],
+  });
+
+  spotDetail.setDataValue("Owner", owner);
+
+  res.json(spotDetail);
+});
 
 //get all the spots
 router.get("/", async (req, res) => {
@@ -73,47 +169,8 @@ router.get("/", async (req, res) => {
   res.json(getAllSpots);
 });
 
-router.get("/current", requireAuth, async (req, res) => {
-  const ownerId = req.user.id;
-  const getAllSpotsByOwner = await Spot.findAll({
-    where: {
-      ownerId,
-    },
-  });
-  let avgRating;
-
-  for (let i = 0; i < getAllSpotsByOwner.length; i++) {
-    let numReviews = await Review.count({
-      where: {
-        spotId: getAllSpotsByOwner[i].id,
-      },
-    });
-    let stars = await Review.sum("stars", {
-      where: {
-        spotId: getAllSpotsByOwner[i].id,
-      },
-    });
-
-    if (stars === null) avgRating = 0;
-    else {
-      avgRating = stars / numReviews;
-    }
-
-    getAllSpotsByOwner[i].setDataValue("avgRating", avgRating);
-
-    const imageUrl = await SpotImage.findOne({
-      where: {
-        spotId: getAllSpotsByOwner[i].id,
-      },
-    });
-
-    if (imageUrl === null) {
-      getAllSpotsByOwner[i].setDataValue("previewImage", null);
-    } else {
-      getAllSpotsByOwner[i].setDataValue("previewImage", imageUrl.url);
-    }
-  }
-  res.json(getAllSpotsByOwner);
+router.post("/", requireAuth, async (req, res) => {
+  const { address, city, state, country } = req.body;
 });
 
 module.exports = router;
