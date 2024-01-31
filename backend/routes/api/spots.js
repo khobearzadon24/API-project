a748a7ceb79fb86af281cbc0b22bd5b51bbcfa12
Khobe Arzadon <khobearzadon@yahoo.com>
2024-01-30 20:42:36 -0800

    creating a post

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -14,6 +14,32 @@ const spot = require("../../db/models/spot");
 
 const router = express.Router();
 
+const validatePost = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .withMessage("Latitude must be within -90 and 90"),
+  check("lng")
+    .exists({ checkFalsy: true })
+    .withMessage("Longitude must be within -180 and 180"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day must be a positive number"),
+];
+
 // make sure user is logged in to view the spots
 // const validateLogin = [
 //   check("credential")
@@ -171,8 +197,27 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.post("/", requireAuth, async (req, res) => {
-  const { address, city, state, country } = req.body;
+//create a spot
+router.post("/", [requireAuth, validatePost], async (req, res) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+
+  const ownerId = req.user.id;
+
+  const spot = await Spot.create({
+    ownerId,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
+
+  res.json(spot);
 });
 
 module.exports = router;
