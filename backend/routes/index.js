352e102a674eb1d051cdfc80b89ac23c14e211c8
Khobe Arzadon <khobearzadon@yahoo.com>
2024-02-16 13:41:49 -0800

    phase 1 and 2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,17 +16,6 @@ if (process.env.NODE_ENV === "production") {
     );
   });
 
-  // Add a XSRF-TOKEN cookie in development
-  if (process.env.NODE_ENV !== "production") {
-    router.get("/api/csrf/restore", (req, res) => {
-      const csrfToken = req.csrfToken();
-      res.cookie("XSRF-TOKEN", csrfToken);
-      res.status(200).json({
-        "XSRF-Token": csrfToken,
-      });
-    });
-  }
-
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/dist")));
 
@@ -37,6 +26,17 @@ if (process.env.NODE_ENV === "production") {
       path.resolve(__dirname, "../../frontend", "dist", "index.html")
     );
   });
+
+  // Add a XSRF-TOKEN cookie in development
+  if (process.env.NODE_ENV !== "production") {
+    router.get("/api/csrf/restore", (req, res) => {
+      const csrfToken = req.csrfToken();
+      res.cookie("XSRF-TOKEN", csrfToken);
+      res.status(200).json({
+        "XSRF-Token": csrfToken,
+      });
+    });
+  }
 }
 
 module.exports = router;
